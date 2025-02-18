551cabc62b743198bb91fa5d70f19383f7f614fb
AJRR133 <anjoserr@gmail.com>
2025-02-18 12:38:02 +0100

    entorno

---
+++ src/entorno/ejercicio5.java
@@ -0,0 +1,10 @@
+package entorno;
+
+public class ejercicio5 {
+	 public static void main(String[] args) {
+		 String palabra = "a";
+		 int longitud = palabra.length();
+		 System.out.println("Longitud de la palabra: " + longitud);
+		 }
+
+}
